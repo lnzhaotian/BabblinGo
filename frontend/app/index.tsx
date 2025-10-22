0e5419776b40cd8c111a5a4d9527c092a24f94d5
赵金海 <lnzhaotian@gmail.com>
2025-10-22 14:48:30 +0900

    chore: initial combined commit for BabblinGo (frontend + backend)

---
+++ frontend/app/index.tsx
@@ -0,0 +1,31 @@
+import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { config } from './config';
+
+export default function Index() {
+  const [data, setData] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get(`${config.apiUrl}/api`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data from API:", error);
+      });
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>{data}</Text>
+    </View>
+  );
+}
